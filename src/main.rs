6432a3d7c1b674e91d7bf064a92700577d4b54a0
Audun Halland <audun.halland@gmail.com>
2021-02-17 04:24:32 +0100

    feat: add I tetromino

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use bevy::prelude::*;
 use bevy::render::pass::ClearColor;
-use bevy_rapier2d::physics::{RapierConfiguration, RapierPhysicsPlugin};
-use bevy_rapier2d::rapier::dynamics::RigidBodyBuilder;
+use bevy_rapier2d::physics::{JointBuilderComponent, RapierConfiguration, RapierPhysicsPlugin};
+use bevy_rapier2d::rapier::dynamics::{BallJoint, RigidBodyBuilder};
 use bevy_rapier2d::rapier::geometry::ColliderBuilder;
+use nalgebra::Point2;
 use rand::Rng;
 
 fn main() {
@@ -18,6 +19,7 @@ fn main() {
         .add_startup_system(setup_game.system())
         .add_startup_system(setup_board.system())
         .add_startup_system(setup_test_blocks.system())
+        .add_startup_system(setup_initial_tetromino.system())
         .add_plugin(RapierPhysicsPlugin)
         .run();
 }
@@ -47,8 +49,8 @@ impl Game {
 impl Default for Game {
     fn default() -> Self {
         Self {
-            n_lanes: 4,
-            n_rows: 4,
+            n_lanes: 8,
+            n_rows: 20,
             tetromino_colors: vec![],
         }
     }
@@ -75,6 +77,7 @@ fn setup_game(mut game: ResMut<Game>, mut materials: ResMut<Assets<ColorMaterial
     ];
 }
 
+#[derive(Clone, Copy)]
 enum TetrominoKind {
     I,
     O,
@@ -101,6 +104,12 @@ impl TetrominoKind {
 
 struct Block;
 
+struct Tetromino {
+    kind: TetrominoKind,
+    blocks: Vec<Entity>,
+    joints: Vec<Entity>,
+}
+
 fn setup_board(
     commands: &mut Commands,
     game: Res<Game>,
@@ -122,29 +131,87 @@ fn setup_board(
         .with(ColliderBuilder::cuboid(game.n_lanes as f32 * 0.5, 0.5));
 }
 
-fn setup_test_blocks(game: Res<Game>, commands: &mut Commands) {
+fn setup_test_blocks(commands: &mut Commands, game: Res<Game>) {
     for lane in 0..game.n_lanes {
-        for row in 0..game.n_rows {
-            spawn_block(commands, &game, lane, row + 5);
+        for row in 0..u8::min(game.n_rows, 2) {
+            let kind = TetrominoKind::random();
+            spawn_block(commands, &game, kind, lane, row + 2);
         }
     }
 }
 
-fn spawn_block(commands: &mut Commands, game: &Game, lane: u8, row: u8) {
-    let kind = TetrominoKind::random();
+fn setup_initial_tetromino(commands: &mut Commands, game: Res<Game>) {
+    spawn_tetromino(commands, &game);
+}
 
+fn spawn_block(
+    commands: &mut Commands,
+    game: &Game,
+    kind: TetrominoKind,
+    lane: u8,
+    row: u8,
+) -> Entity {
     // x, y is the center of the block
     let x = game.left_wall_x() + lane as f32 + 0.5;
     let y = game.floor_y() + row as f32 + 0.5;
 
+    // Game gets more difficult when this is lower:
+    let linear_damping = 3.0;
+
+    let rigid_body = RigidBodyBuilder::new_dynamic()
+        .translation(x, y)
+        .mass(1.0)
+        .linear_damping(linear_damping);
+    let collider = ColliderBuilder::cuboid(0.5, 0.5).density(1.0);
+
     commands
         .spawn(SpriteBundle {
             material: game.tetromino_colors[kind as usize].clone(),
-            // transform: Transform::from_translation(Vec3::new(x, y, 1.0)),
             sprite: Sprite::new(Vec2::new(BLOCK_PX_SIZE, BLOCK_PX_SIZE)),
             ..Default::default()
         })
-        .with(RigidBodyBuilder::new_dynamic().translation(x, y))
-        .with(ColliderBuilder::cuboid(0.5, 0.5).density(100.0))
-        .with(Block);
+        .with(rigid_body)
+        .with(collider)
+        .with(Block)
+        .current_entity()
+        .unwrap()
+}
+
+fn spawn_tetromino(commands: &mut Commands, game: &Game) {
+    let kind = TetrominoKind::I;
+    let lane = game.n_lanes / 2;
+
+    let mut prev_block_entity: Option<Entity> = None;
+    let mut blocks: Vec<Entity> = vec![];
+    let mut joints: Vec<Entity> = vec![];
+
+    for i in 0..4 {
+        let row = game.n_rows - 1 - i;
+        let block_entity = spawn_block(commands, game, kind, lane, row);
+
+        blocks.push(block_entity);
+
+        if let Some(prev_block_entity) = prev_block_entity {
+            let joint = BallJoint::new(Point2::origin(), Point2::new(0.0, 1.0));
+            let joint_entity = commands
+                .spawn((JointBuilderComponent::new(
+                    joint,
+                    prev_block_entity,
+                    block_entity.clone(),
+                ),))
+                .current_entity()
+                .unwrap();
+            joints.push(joint_entity);
+        }
+
+        prev_block_entity = Some(block_entity);
+    }
+
+    let tetromino = Tetromino {
+        kind,
+        blocks,
+        joints,
+    };
+
+    commands.spawn((tetromino,));
 }
